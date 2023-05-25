98f6430808b58b7bb17e8157855eb6b8e6ebc859
doducmanh <manh.doduc@apuscorp.com>
2023-05-25 16:19:26 +0700

    role controller

--- src/main/java/com/fashion/jersey/controller/RoleController.java
+++ src/main/java/com/fashion/jersey/controller/RoleController.java
@@ -3,13 +3,17 @@ package com.fashion.jersey.controller;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.fashion.entity.NotificationEntity;
 import com.fashion.entity.RoleEntity;
+import com.fashion.entity.TypeProductEntity;
 import com.fashion.service.role.RoleServiceImpl;
+import com.fashion.service.typeproduct.TypeProductServiceImpl;
 import com.google.gson.Gson;
 
 @Path(value = "/api/v1/role")
@@ -37,5 +41,42 @@ public class RoleController {
 		Gson gs = new Gson();
 		String data = gs.toJson(role);
 		return data;
+	}
+	
+	
+	@POST
+	@Path(value = "/insert")
+	public String save(String data) {
+		Gson gs = new Gson();
+		RoleEntity lsp = gs.fromJson(data, RoleEntity.class);
+		Boolean themtc = RoleServiceImpl.getNewRole().insert(lsp);
+		NotificationEntity tb = new NotificationEntity();
+		if (themtc) {
+			tb.setMacode(1);
+			tb.setText("Thêm ROLE thành công");
+		} else {
+			tb.setMacode(0);
+			tb.setText("Thêm ROLE thất bại");
+		}
+		String trave = gs.toJson(tb);
+		return trave;
+	}
+	
+	@POST
+	@Path(value = "/update")
+	public String update(String data) {
+		Gson gs = new Gson();
+		RoleEntity lsp = gs.fromJson(data, RoleEntity.class);
+		Boolean themtc = RoleServiceImpl.getNewRole().update(lsp);
+		NotificationEntity tb = new NotificationEntity();
+		if (themtc) {
+			tb.setMacode(1);
+			tb.setText("Sửa ROLE thành công");
+		} else {
+			tb.setMacode(0);
+			tb.setText("Sửa ROLE thất bại");
+		}
+		String trave = gs.toJson(tb);
+		return trave;
 	}
 }
