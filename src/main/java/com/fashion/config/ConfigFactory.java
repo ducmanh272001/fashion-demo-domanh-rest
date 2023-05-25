0e2583d8a2783094efed6c0e1d76e02c42509d6f
doducmanh <manh.doduc@apuscorp.com>
2023-05-25 11:54:51 +0700

    feat: Config Factory

--- src/main/java/com/fashion/config/ConfigFactory.java
+++ src/main/java/com/fashion/config/ConfigFactory.java
@@ -1,21 +1,62 @@
 package com.fashion.config;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.fashion.entity.BillDetailEntity;
+import com.fashion.entity.BillEntity;
+import com.fashion.entity.BranchEntity;
+import com.fashion.entity.CalculateEntity;
+import com.fashion.entity.ColorEntity;
+import com.fashion.entity.CustomerEntity;
+import com.fashion.entity.HotProductEntity;
+import com.fashion.entity.ImagerEntity;
+import com.fashion.entity.NewsEntity;
+import com.fashion.entity.ProductDetailEntity;
+import com.fashion.entity.ProductEntity;
+import com.fashion.entity.RoleEntity;
+import com.fashion.entity.SizeEntity;
+import com.fashion.entity.TypeProductEntity;
+import com.fashion.entity.UserEntity;
+import com.fashion.entity.UserRoleEntity;
+
 public class ConfigFactory {
 
-	
-	private static SessionFactory FACTORY;
-	
-	private ConfigFactory(){
-		
+	private ConfigFactory() {
+
 	}
-	
-	public static SessionFactory getSessionFactory() {
-		if(FACTORY == null) {
-			FACTORY = new Configuration().configure().buildSessionFactory();
+
+	private static final SessionFactory concreteSessionFactory;
+
+	static {
+		try {
+			Properties prop = new Properties();
+			prop.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+			prop.setProperty("hibernate.connection.url",
+					"jdbc:postgresql://ec2-52-54-200-216.compute-1.amazonaws.com:5432/d38ns07vnovqes");
+			prop.setProperty("hibernate.connection.username", "xbhdpcjhaoirrf");
+			prop.setProperty("hibernate.connection.password", "eb4aa3ae352617bdbb27daa404f490613dd44f86c7b1589035e3fe983dcddaf1");
+			prop.setProperty("hibernate.hbm2ddl.auto", "update");
+			prop.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+			prop.setProperty("hibernate.show_sql", "false");
+
+			concreteSessionFactory = new Configuration().addAnnotatedClass(SizeEntity.class)
+					.addAnnotatedClass(TypeProductEntity.class).addAnnotatedClass(BranchEntity.class)
+					.addAnnotatedClass(ProductEntity.class).addAnnotatedClass(ColorEntity.class)
+					.addAnnotatedClass(ProductDetailEntity.class).addAnnotatedClass(UserEntity.class)
+					.addAnnotatedClass(RoleEntity.class).addAnnotatedClass(UserRoleEntity.class)
+					.addAnnotatedClass(CustomerEntity.class).addAnnotatedClass(BillEntity.class)
+					.addAnnotatedClass(BillDetailEntity.class).addAnnotatedClass(NewsEntity.class)
+					.addAnnotatedClass(ImagerEntity.class).addAnnotatedClass(HotProductEntity.class)
+					.addAnnotatedClass(CalculateEntity.class).setProperties(prop).buildSessionFactory();
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
 		}
-		return FACTORY;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return concreteSessionFactory;
 	}
 }
