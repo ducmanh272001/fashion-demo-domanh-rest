e0d4bdaf6c4dac9b45ea8ad4ee656a4951c28dd5
doducmanh <manh.doduc@apuscorp.com>
2023-05-26 16:15:51 +0700

    firt Ok

--- src/main/java/com/fashion/jersey/controller/BranchController.java
+++ src/main/java/com/fashion/jersey/controller/BranchController.java
@@ -3,12 +3,19 @@ package com.fashion.jersey.controller;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.fashion.entity.BranchEntity;
+import com.fashion.entity.HotProductEntity;
+import com.fashion.entity.NotificationEntity;
+import com.fashion.entity.TypeProductEntity;
 import com.fashion.service.branch.BranchServiceImpl;
+import com.fashion.service.hotproduct.HotProductServiceImpl;
+import com.fashion.service.typeproduct.TypeProductServiceImpl;
 import com.google.gson.Gson;
 
 @Path(value = "/api/v1/branch")
@@ -25,6 +32,78 @@ public class BranchController {
 		}
 		Gson gs = new Gson();
 		String data = gs.toJson(list);
+		return data;
+	}
+	
+	
+
+	@GET
+	@Path(value = "/search/{idla}")
+	public String findBrand(@PathParam(value = "idla")int idla) {
+		// Trả về 1 cái String mà giống mvc thôi
+		Gson gs = new Gson();
+		BranchEntity lsp = BranchServiceImpl.getNewBranchEntity().selectById(idla);
+		lsp.setListSanPham(null);
+		String data = gs.toJson(lsp);
+		return data;
+	}
+	
+	@GET
+	@Path(value = "/phan-trang/{idla}")
+	public String phanTrang(@PathParam(value = "idla") int idla) {
+		// Trả về 1 cái String mà giống mvc thôi
+		Gson gs = new Gson();
+		List<BranchEntity> list = BranchServiceImpl.getNewBranchEntity().phanTrang(idla);
+		for (BranchEntity loaiSanPham : list) {
+			loaiSanPham.setListSanPham(null);
+		}
+		String data = gs.toJson(list);
+		return data;
+	}
+
+	@POST
+	@Path(value = "/insert")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String save(String nhanhHieu) {
+		Gson gs = new Gson();
+		BranchEntity hd = gs.fromJson(nhanhHieu, BranchEntity.class);
+		Boolean themtc = BranchServiceImpl.getNewBranchEntity().insert(hd);
+		NotificationEntity tb = new NotificationEntity();
+		if(themtc) {
+			tb.setText("Thêm thành công");
+		}else {
+			tb.setText("Thêm thất bại");
+		}
+		String trave = gs.toJson(tb);
+		return trave;
+	}
+	
+	
+
+	@POST
+	@Path(value = "/update")
+	public String update(String data) {
+		Gson gs = new Gson();
+		BranchEntity lsp = gs.fromJson(data, BranchEntity.class);
+		Boolean themtc = BranchServiceImpl.getNewBranchEntity().update(lsp);
+		NotificationEntity tb = new NotificationEntity();
+		if (themtc) {
+			tb.setMacode(1);
+			tb.setText("Sửa nhãn hiệu thành công");
+		} else {
+			tb.setMacode(0);
+			tb.setText("Sửa nhãn hiệu thất bại");
+		}
+		String trave = gs.toJson(tb);
+		return trave;
+	}
+	
+	@GET
+	@Path(value = "/count")
+	public String countThuongHieu() {
+		Long soluong = BranchServiceImpl.getNewBranchEntity().selectCount();
+		Gson gs = new Gson();
+		String data = gs.toJson(soluong);
 		return data;
 	}
 }
--- src/main/java/com/fashion/service/branch/BranchServiceImpl.java
+++ src/main/java/com/fashion/service/branch/BranchServiceImpl.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 
 import com.fashion.config.ConfigFactory;
 import com.fashion.entity.BranchEntity;
+import com.fashion.entity.TypeProductEntity;
 
 public class BranchServiceImpl implements BranchService<BranchEntity> {
 
@@ -46,8 +47,8 @@ public class BranchServiceImpl implements BranchService<BranchEntity> {
 		Session ss = FACTORY.openSession();
 		try {
 			ss.beginTransaction();
-			BranchEntity nh = (BranchEntity) ss.createQuery("from BranchEntity where id = :idla").setParameter("idla", id)
-					.uniqueResult();
+			BranchEntity nh = (BranchEntity) ss.createQuery("from BranchEntity where id = :idla")
+					.setParameter("idla", id).uniqueResult();
 			return nh;
 		} catch (Exception e) {
 			System.out.println("Xảy ra lỗi câu lệnh truy vấn");
@@ -102,7 +103,8 @@ public class BranchServiceImpl implements BranchService<BranchEntity> {
 		Session ss = FACTORY.openSession();
 		try {
 			ss.beginTransaction();
-			List<BranchEntity> lst = ss.createQuery("from BranchEntity where name_brand = :name").setParameter("name", idtim).list();
+			List<BranchEntity> lst = ss.createQuery("from BranchEntity where name_brand = :name")
+					.setParameter("name", idtim).list();
 			return lst;
 		} catch (Exception e) {
 			System.out.println("Xảy ra lỗi câu lệnh truy vấn");
@@ -131,6 +133,44 @@ public class BranchServiceImpl implements BranchService<BranchEntity> {
 			ss.close();
 		}
 		return false;
+	}
+
+	// Phân trang
+	public List<BranchEntity> phanTrang(int sotrang) {
+		// mỞ sesion ra
+		Session ss = FACTORY.openSession();
+		try {
+			// Bắt đầu 1 phiên làm việc
+			ss.beginTransaction();
+			// Vì là lấy ra dữ liệu nên sẽ ko cần commit
+			List<BranchEntity> lst = ss.createQuery("from BranchEntity order by id desc").setMaxResults(8)
+					.setFirstResult((sotrang - 1) * 8).getResultList();
+			return lst;
+		} catch (Exception e) {
+			System.out.println("Lỗi câu lệnh truy vấn");
+			ss.getTransaction().rollback();
+		} finally {
+			ss.close();
+		}
+		return null;
+	}
+
+	// Số lượng danh mục
+	public Long selectCount() {
+		// Bắt đầu 1 phiên làm việc
+		Session ss = FACTORY.openSession();
+		try {
+			ss.beginTransaction();
+			// Vì trả về 1 cái nên không cần commit
+			List<Long> list = ss.createQuery("select count(*) from BranchEntity").list();
+			return list.get(0);
+		} catch (Exception e) {
+			ss.getTransaction().rollback();
+			System.out.println("Lỗi câu lệnh try vấn");
+		} finally {
+			ss.close();
+		}
+		return null;
 	}
 
 }
