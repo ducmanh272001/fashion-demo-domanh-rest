5a59ceccd77c7d3df80267c4dc3ce33d94d297e0
doducmanh <manh.doduc@apuscorp.com>
2023-05-26 18:24:01 +0700

    Color controller

--- src/main/java/com/fashion/jersey/controller/ColorController.java
+++ src/main/java/com/fashion/jersey/controller/ColorController.java
@@ -3,12 +3,16 @@ package com.fashion.jersey.controller;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 import com.fashion.entity.ColorEntity;
+import com.fashion.entity.NotificationEntity;
+import com.fashion.entity.TypeProductEntity;
 import com.fashion.service.color.ColorServiceImpl;
+import com.fashion.service.typeproduct.TypeProductServiceImpl;
 import com.google.gson.Gson;
 
 import javax.ws.rs.Produces;
@@ -29,6 +33,24 @@ public class ColorController {
 		return data;
 	}
 	
+	
+	@POST
+	@Path(value = "/insert")
+	public String insertColor(String data) {
+		Gson gs = new Gson();
+		ColorEntity lsp = gs.fromJson(data, ColorEntity.class);
+		Boolean themtc = ColorServiceImpl.getNewColorEntity().insert(lsp);
+		NotificationEntity tb = new NotificationEntity();
+		if (themtc) {
+			tb.setMacode(1);
+			tb.setText("Thêm màu sắc thành công");
+		} else {
+			tb.setMacode(0);
+			tb.setText("Thêm màu sắc thất bại");
+		}
+		String trave = gs.toJson(tb);
+		return trave;
+	}
 	
 	//Tìm theo tên màu sắc
 	@GET
